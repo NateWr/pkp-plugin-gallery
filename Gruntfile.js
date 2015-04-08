cca039cfdd68f2e3d5808b523cf2832e407e8c35
Nate Wright <natew@notthisway.com>
2015-04-08 11:19:12 +0100

    * Setup grunt for LESS/JS processing

---
+++ Gruntfile.js
@@ -0,0 +1,79 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+
+		// Load grunt project configuration
+		pkg: grunt.file.readJSON('package.json'),
+
+		// LESS CSS compiler
+		less: {
+			build: {
+				options: {
+					cleancss: true,
+					ieCompat: true
+				},
+				files: {
+					'assets/css/admin.css': 'assets/src/less/admin.less'
+				}
+			}
+		},
+
+		// JSHint
+		jshint: {
+			test: {
+				src: 'assets/src/js/*.js'
+			}
+		},
+
+		// Concatenate scripts
+		concat: {
+			build: {
+				files: {
+					'assets/js/submission-form.js': [
+						'assets/src/js/submission-form.js',
+						'assets/src/js/submission-form-*.js'
+					]
+				}
+			}
+		},
+
+		// Minimize scripts
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			build: {
+				files: {
+					'assets/js/submission-form.min.js' : 'assets/js/submission-form.js'
+				}
+			}
+		},
+
+		// Auto-compile changes
+		watch: {
+			less: {
+				files: ['assets/src/less/*.less'],
+				tasks: ['less']
+			},
+			js: {
+				files: ['assets/src/js/*.js'],
+				tasks: ['jshint', 'concat', 'uglify']
+			}
+		}
+
+	});
+
+	// Load tasks
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	// Default task(s).
+	grunt.registerTask('default', ['watch']);
+
+};
