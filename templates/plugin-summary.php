c8f8ad9584f3bb3978a729ae3aac970794e121f8
Nate Wright <natew@notthisway.com>
2015-05-15 12:38:23 +0100

    * Show updates and indicate plugin status to maintainers

--- templates/plugin-summary.php
+++ templates/plugin-summary.php
@@ -8,18 +8,51 @@
  */
 ?>
 
-<div class"plugin">
+<div class="plugin <?php echo esc_attr( $plugin->post_status ); ?>">
     <div class="title">
+
+        <?php if ( $plugin->post_status == 'update' ) : ?>
+        <span class="dashicons dashicons-update"></span>
+        <span class="status-notice">
+            <?php esc_html_e( 'Update: ', 'pkp-plugin-gallery' ); ?>
+        </span>
+
+        <?php elseif ( $plugin->post_status == 'submission' ) : ?>
+        <span class="dashicons dashicons-download"></span>
+        <span class="status-notice">
+            <?php esc_html_e( 'Submission: ', 'pkp-plugin-gallery' ); ?>
+        </span>
+
+        <?php elseif ( $plugin->post_status == 'disable' ) : ?>
+        <span class="dashicons dashicons-shield-alt"></span>
+        <span class="status-notice">
+            <?php esc_html_e( 'Disabled: ', 'pkp-plugin-gallery' ); ?>
+        </span>
+        <?php endif; ?>
+
         <?php echo $plugin->name; ?>
     </div>
     <div class="actions">
         <a href="<?php echo get_the_permalink( $plugin->ID ); ?>">
             <?php esc_html_e( 'View', 'pkp-plugin-gallery' ); ?>
         </a>
-        <?php if ( pkp_is_author( $plugin->ID ) ) : ?>
+        <?php if ( pkp_is_author( $plugin->ID ) && $plugin->post_status !== 'update' ) : ?>
         <a href="<?php echo esc_url( get_the_permalink( $plugin->ID ) . 'edit' ); ?>">
             <?php esc_html_e( 'Edit', 'pkp-plugin-gallery' ); ?>
         </a>
         <?php endif; ?>
     </div>
+
+    <?php if ( pkp_is_author( $plugin->ID ) ) : $plugin->load_updates(); ?>
+        <?php if ( !empty( $plugin->updates ) ) : ?>
+            <ul class="updates">
+                <?php foreach( $plugin->updates as $update ) : ?>
+                <li>
+                    <?php $update->print_summary_view(); ?>
+                </li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+    <?php endif; ?>
+
 </div>
